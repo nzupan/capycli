89ef1d942654a27b4f73291ce591e77af608aabd
Nejc Zupan <nejc.zupan@siemens.com>
2023-09-05 12:36:24 +0200

    fix: upload correct attachment if current one has .git extension

--- capycli/bom/create_components.py
+++ capycli/bom/create_components.py
@@ -279,6 +279,9 @@ class BomCreateComponents(capycli.common.script_base.ScriptBase):
                 print_yellow(
                     "    WARNING: SW360 source URL", release_data["sourceCodeDownloadurl"],
                     "differs from BOM URL", data["sourceCodeDownloadurl"])
+                update_data["sourceCodeDownloadurl"] = data["sourceCodeDownloadurl"]
+                print_yellow(
+                    "        Proceeding with uploading correct attachment")
 
         if "binaryDownloadurl" in data and data["binaryDownloadurl"]:
             if not release_data.get("binaryDownloadurl", ""):
@@ -343,7 +346,7 @@ class BomCreateComponents(capycli.common.script_base.ScriptBase):
             filename = CycloneDxSupport.get_ext_ref_binary_file(cx_comp)
             filehash = CycloneDxSupport.get_binary_file_hash(cx_comp)
 
-        # Note that we retreive the SHA1 has from the CycloneDX data.
+        # Note that we retrieve the SHA1 has from the CycloneDX data.
         # But there is no guarantee that this *IS* really a SHA1 hash!
 
         if (filename is None or filename == '') and url:
@@ -373,6 +376,10 @@ class BomCreateComponents(capycli.common.script_base.ScriptBase):
                     print_yellow(
                         "    WARNING: different source attachment - BOM:",
                         filename, "SW360:", attachment["filename"])
+                    if attachment["filename"].endswith('.git'):
+                        source_attachment_exists = False
+                        print_yellow(
+                            "        Existing attachment has .git extension. Upload new archive attachment ", filename)
                 elif filehash and attachment["sha1"] != filehash:
                     print_yellow(
                         "    WARNING: different hash for source attachment", filename,
